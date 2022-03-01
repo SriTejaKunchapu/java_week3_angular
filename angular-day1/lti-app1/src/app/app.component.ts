c81e5212e2d48d88a5ce6341d241aceaa2d76561
SriTejaKunchapu <44490340+SriTejaKunchapu@users.noreply.github.com>
2022-03-01 13:38:06 +0530

    Add files via upload

---
+++ angular-day1/lti-app1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+//metadata 
+@Component({
+  selector: 'app-root', 
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] 
+})
+
+//model 
+export class AppComponent {
+  title = 'LTI-Fullstack++ trianing ';
+}
